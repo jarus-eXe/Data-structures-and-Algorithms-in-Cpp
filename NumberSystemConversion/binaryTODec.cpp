55308e02c18978f64eded91f3c1f22e09f8dcd15
Suraj Bar <116182297+jarusxd@users.noreply.github.com>
2023-08-29 00:34:06 +0530

    Create binaryTODec.cpp

---
+++ NumberSystemConversion/binaryTODec.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int binTodec(int n)
+{
+	int ans = 0;
+	int i = 0;
+	
+	
+	while(n!=0){
+		int digit = n % 10;
+		if(digit == 1){
+			ans = ans + pow(2,i);
+		}
+		n = n / 10;
+		i++;
+	}	
+	return ans;
+}
+int main(){
+	int n;
+	cin>>n;
+	int ans = binTodec(n);
+	
+	cout << "Answer: " << ans << endl;
+	return 0;
+}
+/*
+Output:
+100000
+Answer: 32
+*/
